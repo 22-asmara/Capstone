108034e77090490726e0299481374faa259b1761
Asmara <75630774+22-asmara@users.noreply.github.com>
2020-12-22 12:01:12 +0500

    Add files via upload

---
+++ Loginuser/app/src/main/java/com/example/loginuser/SignUp.java
@@ -0,0 +1,22 @@
+package com.example.loginuser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class SignUp extends AppCompatActivity {
+
+    Button SignUp;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+
+
+
+    }
+}
